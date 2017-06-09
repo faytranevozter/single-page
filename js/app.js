fa8a74406db718a8852221527c48d32abf012246
Fahrur Rifai <faimaknyus@gmail.com>
2017-06-09 20:22:38 +0700

    scroll to free trial

--- js/app.js
+++ js/app.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
 		// autoplay_slideshow:true
 		social_tools: ''
 	});
+
+	// goto free trial
+	$('a[href=#free-trial]').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#section7').offset().top
+		}, 700, function(){
+			$('#big-input-email').focus();
+		});
+	});
 });
