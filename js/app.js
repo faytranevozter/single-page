2a1ba7b61fd228a8f3670b35691c999eb63706ee
Fahrur Rifai <faimaknyus@gmail.com>
2017-07-10 22:10:23 +0700

    update

--- js/app.js
+++ js/app.js
@@ -7,11 +7,49 @@ $(document).ready(function(){
 	});
 
 	// goto free trial
-	$('a[href=#free-trial]').click(function(){
+	$("a[href='#free-trial']").click(function(){
 		$('html, body').animate({
 			scrollTop: $('#section7').offset().top
 		}, 700, function(){
 			$('#big-input-email').focus();
 		});
 	});
+
+	// sharer url
+	var currentUrl = $(location).attr('href');
+	if ($('.post-share-box a.bg-facebook').length > 0) {
+		var urlShareFb = 'https://www.facebook.com/sharer/sharer.php?u='+ encodeURIComponent(currentUrl);
+		$('.post-share-box a.bg-facebook').attr('href', urlShareFb);
+	}
+	if ($('.post-share-box a.bg-google').length > 0) {
+		var urlShareGplus = 'https://plus.google.com/share?url='+ encodeURIComponent(currentUrl);
+		$('.post-share-box a.bg-google').attr('href', urlShareGplus);
+	}
+	if ($('.post-share-box a.bg-twitter').length > 0) {
+		var urlShareTwitter = 'http://twitter.com/share?url='+ encodeURIComponent(currentUrl) +'&text='+ $('head title').text();
+		$('.post-share-box a.bg-twitter').attr('href', urlShareTwitter);
+	}
+	$('.post-share-box a').click(function(){
+		PopupCenter(this.href, 'Share', 600, 400);
+		return false;
+	});
+
 });
+
+function PopupCenter(url, title, w, h) {
+	// Fixes dual-screen position                         Most browsers      Firefox
+	var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+	var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+	var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+	var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+	var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+	var top = ((height / 2) - (h / 2)) + dualScreenTop;
+	var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+	// Puts focus on the newWindow
+	if (window.focus) {
+		newWindow.focus();
+	}
+}
